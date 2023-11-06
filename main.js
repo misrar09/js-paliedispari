93f9ae647ed2e9370ea5a2851a33c9827a25543d
misrar09 <misrar09@gmail.com>
2023-11-06 18:48:37 +0100

    OddEven Done

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
+//PALANIDROME EXCERCISE SECTION
+
 //User Input
-const userInput = prompt("Please write a Word");
+const userWord = prompt(" This is Palandrome Game: Please write a Word");
 
 //Funtion to reverse the string by using split, reverse and join properties of the array
 function reverseWord(word) {
@@ -16,16 +18,76 @@ function reverseWord(word) {
  
 
 //Checking if the the user inserted word is a Palindorem or not
-if(userInput == "" || userInput == null){
+if(userWord == "" || userWord == null){
 
     alert("Please insert a Word")
 }
 
-else if(userInput == reverseWord(userInput)){
+else if(userWord == reverseWord(userWord)){
     
-    document.getElementById("palindrome").innerHTML = `${userInput}, "This is a Palindrome Word"` 
+    document.getElementById("palindrome").innerHTML = `"${userWord}", This is a Palindrome Word` 
 }
 else{
-    document.getElementById("palindrome").innerHTML = `${userInput}, "This is not a Palindrome Word, Pleae try again"`
+    document.getElementById("palindrome").innerHTML = `"${userWord}", This is not a Palindrome word, Pleae try again!`
 }
+
+//***************************************** */
+
+
+
+const options = document.getElementById("evenOdd"); // Correct the ID for the select element
+const userInput = document.getElementById("user_input");
+const yourNumber = document.querySelector(".your_number");
+const computerNumber = document.querySelector(".computer_number");
+const btnEnter = document.getElementById("btn");
+const winner = document.getElementById("winner");
+winner.style.fontSize = "2rem";
+winner.style.color = "Green";
+
+btnEnter.addEventListener("click", function () {
+    const evenOdd = options.value;
+    const userNumber = parseInt(userInput.value);
+    let canContinue = true; // Boolean flag to control the flow
+
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+        alert("Please insert values from 1 to 5");
+        canContinue = false; // Set the flag to false to stop execution
+    }
+
+    if (canContinue) {
+        yourNumber.innerHTML = `Your Number: ${userNumber}`;
+
+        function computerNum(randomNum) {
+            return Math.floor(Math.random() * 5) + 1;
+        }
+
+        const randomNumber = computerNum();
+        computerNumber.innerHTML = `Computer's Number: ${randomNumber}`;
+
+        if (canContinue) {
+            const total = userNumber + randomNumber;
+            let result = "";
+
+            if (total % 2 === 0) {
+                result = "Even";
+            } else {
+                result = "Odd";
+            }
+
+            if ((total % 2 === 0 && evenOdd === "even") || (total % 2 !== 0 && evenOdd === "odd")) {
+                winner.innerHTML = `You Won. Total is ${result}`;
+            } else {
+                winner.innerHTML = `You Lost!!!. Total is ${result}`;
+            }
+        } else {
+            // Clear output elements if the flag is false
+            yourNumber.innerHTML = "";
+            computerNumber.innerHTML = "";
+            winner.innerHTML = "";
+        }
+    }
+});
+
+
+
 
